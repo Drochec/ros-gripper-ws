2eb916f6027666fba66aacc80ebb22455db2d1b9
Jan Drochýtek <hdspawn104@gmail.com>
2025-08-14 14:40:12 +0200

    Uprava control node a prvni pokus o kalibraci serva

--- ros_ws/src/adc_readout/adc_readout/adc_readout.py
+++ ros_ws/src/adc_readout/adc_readout/adc_readout.py
@@ -1,39 +1,40 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionClient
 
 import std_msgs.msg
-
-from action_interface.action import GripperMove
-
 
 from ads1015 import ADS1015
 import smbus2
 
+from rclpy.action import ActionClient
+from action_interface.action import GripperMove
+
 #Parametry pro převod
-adc_range = 6.144
+ADC_RANGE = 6.144
 
-angle_channel = "in0/gnd"
-current_channel = "in2/gnd"
+ANGLE_CHANNEL = "in0/gnd"
+CURRENT_CHANNEL = "in2/gnd"
 
-i2c_bus = smbus2.SMBus(5)
+I2C_BUS = smbus2.SMBus(5)
 
-ads1115 = ADS1015(i2c_dev=i2c_bus)
+ads1115 = ADS1015(i2c_dev=I2C_BUS)
 ads1115.set_mode("single")
-ads1115.set_programmable_gain(adc_range)
+ads1115.set_programmable_gain(ADC_RANGE)
 
 #Proudove cidlo
 
-sensitivity = 0.185
-samples = 100
+SENSITIVITY = 0.185
+SAMPLES = 100
 
 #current_offset = 0.347
-current_offset = adc_range/2
+current_offset = ADC_RANGE/2
 
 #Uhel okrajove hodnoty
 angle_Vmin = 0.23
 angle_Vmax = 2.98
 
+CLOSED_POS = 0
+OPEN_POS = 170
 
 class adcNode(Node):
 
@@ -53,17 +54,17 @@ class adcNode(Node):
 
         self.calibrate_current_sens()
 
-        self.timer = self.create_timer(0.25, self.publish_readings)
-        self.current_readout_publisher = self.create_publisher(std_msgs.msg.Float32, "adc_current", 10)
-        self.angle_readout_publisher = self.create_publisher(std_msgs.msg.Int32, "adc_angle", 10)
+        self.timer = self.create_timer(0.05, self.publish_readings)
+        self.current_readout_publisher = self.create_publisher(std_msgs.msg.Float32, "adc_current_raw", 10)
+        self.angle_readout_publisher = self.create_publisher(std_msgs.msg.Int32, "adc_angle_raw", 10)
         
-        self.gripper_action_client = self.ActionClient(self, GripperMove, "gripper_move")
+        self.gripper_action_client = ActionClient(self, GripperMove, "gripper_move")
 
     def read_values(self):
-        current_raw = ads1115.get_voltage(current_channel)
+        current_raw = ads1115.get_voltage(CURRENT_CHANNEL)
         self.current = (current_raw - self.current_offset) / self.sensitivity
 
-        angle_raw = ads1115.get_voltage(angle_channel)
+        angle_raw = ads1115.get_voltage(ANGLE_CHANNEL)
         self.angle = (angle_raw - self.angle_Vmin) * 180 / (self.angle_Vmax - self.angle_Vmin)
 
         #self.get_logger().info(str(current_raw))
@@ -75,10 +76,9 @@ class adcNode(Node):
     def calibrate_current_sens(self):
         sum_current_raw = 0.0
         for i in range(self.samples):
-            sum_current_raw += ads1115.get_voltage(current_channel)
+            sum_current_raw += ads1115.get_voltage(CURRENT_CHANNEL)
         self.current_offset = sum_current_raw / self.samples
         self.get_logger().info("Current calbritated - offset: {} A".format(self.current_offset))
-
 
     def calibrate_angle_sens(self):
         action_msg = GripperMove.Goal()
@@ -90,7 +90,7 @@ class adcNode(Node):
 
         sum_angle_raw = 0
         for i in range(self.samples):
-            sum_angle_raw += ads1115.get_voltage(angle_channel)
+            sum_angle_raw += ads1115.get_voltage(ANGLE_CHANNEL)
         self.angle_Vmin = int(sum_angle_raw / self.samples)
 
         action_msg.action = 180
@@ -100,7 +100,7 @@ class adcNode(Node):
 
         sum_angle_raw = 0
         for i in range(self.samples):
-            sum_angle_raw += ads1115.get_voltage(angle_channel)
+            sum_angle_raw += ads1115.get_voltage(ANGLE_CHANNEL)
         self.angle_Vmax = int(sum_angle_raw / self.samples)
 
 
@@ -130,7 +130,7 @@ class adcNode(Node):
 def main(args=None):
 
     rclpy.init(args=args)
-    node = adcNode(current_sens_sensitivity=sensitivity, current_calibration_samples=samples, angle_Vmin=angle_Vmin, angle_Vmax=angle_Vmax)
+    node = adcNode(current_sens_sensitivity=SENSITIVITY, current_calibration_samples=SAMPLES, angle_Vmin=angle_Vmin, angle_Vmax=angle_Vmax)
     rclpy.spin(node)
     rclpy.shutdown()
 
--- ros_ws/src/gripper_servo_controller/gripper_servo_controller/control_node.py
+++ ros_ws/src/gripper_servo_controller/gripper_servo_controller/control_node.py
@@ -8,24 +8,25 @@ from action_interface.action import GripperMove
 
 import piservo
 
-from enum import Enum
-
 #Parametry serva
-servo_signal_pin = 13 #pin musi podporovat HW PWM - viz RasPi pinout (pinout.xyz)
-servo_min_pos = 0 #mozna bude stacit nastavit meze tady
-servo_max_pos = 180 
-servo_min_pulse = 0.5 #Z datasheetu vyrobce
-servo_max_pulse = 2.5
-servo_frequency = 50
+SERVO_SIGNAL_PIN = 13 #pin musi podporovat HW PWM - viz RasPi pinout (pinout.xyz)
+SERVO_MIN_POS = 0 #mozna bude stacit nastavit meze tady
+SERVO_MAX_POS = 180 
+SERVO_MIN_PULSE = 0.5 #Z datasheetu vyrobce
+SERVO_MAX_PULSE = 2.5
+SERVO_FREQUENCY = 50
 
-servo = piservo.Servo(servo_signal_pin, servo_min_pos, servo_max_pos, servo_min_pulse, servo_max_pulse, servo_frequency)
+servo = piservo.Servo(SERVO_SIGNAL_PIN, SERVO_MIN_POS, SERVO_MAX_POS, SERVO_MIN_PULSE, SERVO_MAX_PULSE, SERVO_FREQUENCY)
 
+#Specialni pripady pozice prijate pozice
+CLOSED = -1
+OPEN = -2
 
-class ServoPos(Enum):
-    CLOSED = -1
-    OPEN = -2
+#Horni a spodni limit pozice pro otevreni/zavreni gripperu
+#Nechceme rozervat nastavec.... znova
 
-
+OPEN_ANGLE = 170
+CLOSED_ANGLE = 0
 
 #Ridici Node pro ovladani gripperu
 #Topicy:
@@ -35,67 +36,67 @@ class ServoPos(Enum):
 #Actions - Move - open/close
 
 class controlNode(rclpy.node.Node):
-    
 
 
-    def __init__(self,lower_angle_limit,upper_angle_limit):
-
+    def __init__(self):
 
         super().__init__("control_node")
         
-        #Horni a spodni limit pozice pro otevreni/zavreni gripperu
-        #Nechceme rozervat nastavec.... znova
-        self.lower_angle_limit = lower_angle_limit
-        self.upper_angle_limit = upper_angle_limit
-
-        self.open_angle = 170
-        self.close_angle = 0
 
         self.overcurrent_start = None
         self.overload_active = False
         self.overcurrent_threshold = 0.5   # amps
-        self.overcurrent_duration = 0.1    # seconds
-        self.stop_angle_offset = 0         # example
+        self.overcurrent_duration = 0.05    # seconds
 
-        self.servo_angle = 0
-        self.stop_angle_offset = 1
-        self.stop_current = 0.4
+        self.samples = 100
+
+        self.current = 0.0
+        self.current_offset = 2.3
+        self.current_sensitivity = 0.185
+        
+        self.set_angle = 0
+        self.servo_angle = 0.0
+        self.stop_angle_offset = 0
+
+        self.angle_Vmin = 0.23
+        self.angle_Vmax = 2.98
 
 
         self.timer = self.create_timer(1.0, self.publish_pos) #Kazdou sekundu posle nastavenou pozici
-        self.cmd_pos_pub = self.create_publisher(std_msgs.msg.Int32, "cur_pos", 10)
-        self.cmd_pos_sub = self.create_subscription(std_msgs.msg.Int32, "cmd_pos", self.receive_pos, 10) #Pri poslani dat na topic posune na zadanou pozici
+        self.cmd_pos_pub = self.create_publisher(std_msgs.msg.Int32, "cur_pos", 5)
+        self.cmd_pos_sub = self.create_subscription(std_msgs.msg.Int32, "cmd_pos", self.receive_pos, 5) #Pri poslani dat na topic posune na zadanou pozici
 
         self.current_sub = self.create_subscription(std_msgs.msg.Float32, "adc_current", self.gripper_watchdog, 10)
         self.angle_sub = self.create_subscription(std_msgs.msg.Int32, "adc_angle", self.store_angle, 10)
 
+        #self.adc_sub = self.create_subscription(std_msgs.msg.Float32MultiArray, "adc_data", self.read_sens, 10)
+
         self.action_server = rclpy.action.ActionServer(self, GripperMove, 'gripper_move', self.gripper_move_action)
 
         self.get_logger().info("Control node started")
-        self.move_servo(self.close_angle)
-        self.get_logger().info("Default position set")
+        self.calibrate_sens()
 
     def publish_pos(self):
         msg = std_msgs.msg.Int32()
-        msg.data = self.angle
+        msg.data = self.set_angle
         
         self.cmd_pos_pub.publish(msg)
     
-    def receive_pos(self, pos: std_msgs.msg.Int32):
-        self.get_logger().info("Received pos: " + str(pos.data))
+    def receive_pos(self, angle: std_msgs.msg.Int32):
+        self.get_logger().info("Received pos: " + str(angle.data))
         self.reset_overload()
-        self.angle = pos.data
-        servo.write(pos.data)
+        self.set_angle = angle.data
+        self.move_servo(angle.data)
 
     def move_servo(self, angle):
         self.get_logger().info('Moving gripper....')
         
-        if angle > self.upper_angle_limit:
-            angle = 180
-        elif angle < self.lower_angle_limit:
-            angle = 0
+        if angle > OPEN_ANGLE:
+            angle = OPEN_ANGLE
+        elif angle < CLOSED_ANGLE:
+            angle = CLOSED_ANGLE
 
-        self.angle = angle
+        self.set_angle = angle
         servo.write(angle)
         self.get_logger().info('Set desired servo angle to: {}....'.format(angle))
 
@@ -108,12 +109,12 @@ class controlNode(rclpy.node.Node):
 
         #Specialni pripady => -1 zavrit, -2 otevrit
 
-        if (received_angle == ServoPos.CLOSED):
-            received_angle = self.close_angle
-            print(received_angle)
-        elif (received_angle == ServoPos.OPEN):
-            received_angle = self.open_angle
-            print(received_angle)
+        if (received_angle == CLOSED):
+            received_angle = CLOSED_ANGLE
+            self.get_logger().info(received_angle)
+        elif (received_angle == OPEN):
+            received_angle = OPEN_ANGLE
+            self.get_logger().info(received_angle)
 
         self.reset_overload()
 
@@ -147,12 +148,25 @@ class controlNode(rclpy.node.Node):
         self.overload_active = False
         self.overcurrent_start = None
 
-    def store_angle(self, angle_msg):
-        self.servo_angle = angle_msg.data
+    #def read_sens(self, raw_data: std_msgs.msg.Float32MultiArray):
+    #    current_raw = raw_data.data[0]
+    #    angle_raw = raw_data.data[1]
+    #    
+    #    self.current = (current_raw - self.current_offset) / self.current_sensitivity
+    #    self.servo_angle = int((angle_raw - self.angle_Vmin) * 180 / (self.angle_Vmax - self.angle_Vmin))
+
+    #def calibrate_sens(self):
+    #    self.get_logger().info('Calibrating sensors')
+#
+#        sum_current_raw = 0.0
+    #    for i in range(self.samples):
+            
+    #    self.current_offset = sum_current_raw / self.samples
+        
 
 def main(args=None):
     rclpy.init(args=args)
-    node = controlNode(lower_angle_limit=0,upper_angle_limit=180)
+    node = controlNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
